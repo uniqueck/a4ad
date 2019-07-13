126318d1375533bb9ec5dbbc043972c38d33f0ef
Constantin Krüger <Constantinidis@web.de>
2019-07-14 01:44:56 +0200

    create spring boot project

---
+++ src/main/java/com/github/a4ad/A4adApplication.java
@@ -0,0 +1,13 @@
+package com.github.a4ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A4adApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(A4adApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/a4ad/A4adApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.a4ad;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class A4adApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
