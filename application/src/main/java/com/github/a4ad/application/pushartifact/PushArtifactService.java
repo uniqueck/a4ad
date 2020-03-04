3fc02f61e9eb75a0c98c7c3ca9b88c55bca9429a
Constantin Krüger <Constantinidis@web.de>
2020-03-04 23:01:11 +0100

    add dummy implementation for usecase pushartifact

---
+++ application/src/main/java/com/github/a4ad/application/pushartifact/PushArtifactService.java
@@ -0,0 +1,13 @@
+package com.github.a4ad.application.pushartifact;
+
+import com.github.a4ad.common.UseCase;
+import com.github.a4ad.port.in.pushartifact.PushArtifactUseCase;
+
+@UseCase
+class PushArtifactService implements PushArtifactUseCase {
+
+    @Override
+    public void saveArtifact(PushArtifactUseCaseCommand command) {
+        // implement later
+    }
+}
