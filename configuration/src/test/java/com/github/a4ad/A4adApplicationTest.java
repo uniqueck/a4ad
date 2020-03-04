ea7c6a4aa83e7649dc5e31d41ae7eaad8cced47c
Constantin Krüger <Constantinidis@web.de>
2020-03-04 01:12:40 +0100

    fix technical depts

--- configuration/src/test/java/com/github/a4ad/A4adApplicationTest.java
+++ configuration/src/test/java/com/github/a4ad/A4adApplicationTest.java
@@ -1,8 +1,7 @@
-package a4ad;
+package com.github.a4ad;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.SpringApplicationExtensionsKt;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
